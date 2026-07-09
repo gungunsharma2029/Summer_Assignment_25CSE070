7167f8b39b65c15e65a23771b95cc338328e22e5
gungunsharma2029 <gungun.sharma2025@glbajajgroup.org>
2026-07-09 14:56:55 +0530

    Add files via upload

---
+++ Day-3-Programs/c4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, x, y, temp, gcd, lcm;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    
+    x = a; y = b;
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    gcd = a;
+    lcm = (x * y) / gcd;
+    
+    printf("LCM = %d\n", lcm);
+    return 0;
+}
